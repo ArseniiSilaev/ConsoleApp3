61d146de812a2ddc7ebd919007eac866722fa2e5
ArseniiSilaev <Senyasilaev@yandex.ru>
2021-12-23 11:47:55 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество золота: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько кристаллов по цене 15 Вы хотите купить?: ");
+            int Crystal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("У вас осталось " + (N - 15 * Crystal) + " золота и " + Crystal + " кристаллов");
+
+        }
+    }
+}
