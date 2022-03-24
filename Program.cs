28fd99b1f1bcdb9ddf3ca61f043acf88e8d50251
ArseniiSilaev <96460548+ArseniiSilaev@users.noreply.github.com>
2022-03-24 18:25:14 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,9 +12,11 @@ namespace ConsoleApp3
         {
             Console.Write("Введите количество золота: ");
             int goldCount = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Сколько кристаллов по цене 15 Вы хотите купить?: ");
+            int crystalPrice = 15;
+            Console.Write("Сколько кристаллов по цене " + crystalPrice + " Вы хотите купить ?: ");
             int crystalCount = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("У вас осталось " + (goldCount - 15 * crystalCount) + " золота и " + crystalCount + " кристаллов");
+            goldCount -= crystalPrice * crystalCount;
+            Console.WriteLine("У вас осталось " + goldCount + " золота и " + crystalCount + " кристаллов");
         }
     }
 }
